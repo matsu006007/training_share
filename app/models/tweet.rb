130b0f9434206d4cf80d72ec349f9c458d4f6924
matsu006007 <76504047+matsu006007@users.noreply.github.com>
2021-03-25 13:42:44 +0900

    バリデーション記述

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,11 @@
 class Tweet < ApplicationRecord
+
+  with_options presence: true do
+    validates :title
+    validates :content
+    validates :genre_id, numericality: { other_than: 1 }
+    validates :user
+  end
+
+  belongs_to :user
 end
