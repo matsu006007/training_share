de71e3c3915fd5fee6196dc101a31fb5415a8499
matsu006007 <76504047+matsu006007@users.noreply.github.com>
2021-03-25 18:28:29 +0900

    TrainingFrequencyモデル　記述

---
+++ app/models/training_frequency.rb
@@ -0,0 +1,13 @@
+class TrainingFrequency < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '特に決めていない' },
+    { id: 3, name: '週に1〜3回' },
+    { id: 4, name: '週に4回以上' },
+    { id: 5, name: '月に1〜3回' },
+    { id: 6, name: '月に4回以上' },
+    { id: 7, name: '毎週土or日のどちらか' },
+    { id: 8, name: '毎週土日' },
+    { id: 9, name: 'ほぼ毎日' }
+  ]
+  end
