00479fbf78fabdf976a52b99e156326376309951
matsu006007 <76504047+matsu006007@users.noreply.github.com>
2021-03-25 16:29:28 +0900

    ストロングパラメーター設定

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,16 @@
 class TweetsController < ApplicationController
+  before_action :Login_check
 
   def index
     @tweets = Tweet.includes(:user).order("created_at DESC")
   end
+
+  private
+
+  def Login_check
+    unless user_signed_in?
+      redirect_to root_path
+    end
+  end
+
 end
