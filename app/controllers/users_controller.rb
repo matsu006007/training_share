bf610a8c830a3a38cd427522a3861595dc1f25f6
matsu006007 <76504047+matsu006007@users.noreply.github.com>
2021-03-26 16:34:28 +0900

    usersコントローラー　ページネーション　記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @tweets = Tweet.includes(:user).order("created_at DESC")
+    @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(5)
   end
 end
