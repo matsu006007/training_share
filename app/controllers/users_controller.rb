38eb61a977e565d032f8e33e2073b9ec453ff3a7
matsu006007 <76504047+matsu006007@users.noreply.github.com>
2021-04-02 09:11:52 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
+  before_action :Login_check
 
   def show
     @user = User.find(params[:id])
-    @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(5)
+    @tweets = @user.tweet.order("created_at DESC").page(params[:page]).per(5)
+  end
+end
+
+private
+
+def Login_check
+  unless user_signed_in?
+    redirect_to root_path
   end
 end
